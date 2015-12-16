a20215f8bc513a26ebc4831469b0cbad17a633db
Mike Skalandunas <mskalandunas@gmail.com>
2015-12-16 15:07:35 -0800

    add scenes router

---
+++ routes/scenes-routes.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var Scene = require(__dirname + '/../models/scene');
+var handleError = require(__dirname + '/../lib/handle-server-error');
+var timeAdded = require(__dirname + '/../lib/time-added');
+
+var scenesRouter = module.exports = exports = express.Router();
+
+scenesRouter.get('/scenes', function(req, res) {
+  Scene.find({}, function(err, data) {
+    if (err) return handleError(err, res);
+
+    res.json(data);
+  });
+});
+
+scenesRouter.post('/scenes', bodyParser.json(), function(req, res) {
+  var newScene = new Scene(req.body);
+  newScene.save(function(err, data) {
+    if (err) return handleError(err, res);
+
+    timeAdded();
+    res.json(data);
+  });
+});
+
+scenesRouter.put('/scenes/:id', bodyParser.json(), function(req, res) {
+  var sceneData = req.body;
+  delete sceneData._id;
+  Scene.update({_id: req.params.id}, sceneData, function(err) {
+    if (err) return handleError(err, res);
+
+    res.json({msg: 'Scene updated.'});
+  });
+});
+
+scenesRouter.delete('/scenes/:id', function(req, res) {
+  Scene.remove({_id: req.params.id}, function(err) {
+    if (err) return handleError(err, res);
+
+    res.json({msg: 'Scene removed.'});
+  });
+});
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 'use strict';
 
+var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
+var scenesRouter = require(__dirname + '/routes/scenes-routes');
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/scene_dev');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/scene_stream_dev');
 
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/build'));
+
+app.use('/api', scenesRouter)
 
 app.listen(port, function() {
   console.log('Listening at ' + port + '.');
