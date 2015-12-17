cc606af7b5362aed8466052341e7f163459c284a
Amanda Keogh <amanda.keogh@gmail.com>
2015-12-17 13:39:36 -0800

    add initial choices directive

---
+++ app/js/scenes/directives/choices_directive.js
@@ -0,0 +1,11 @@
+module.exports = exports = function(app) {
+  app.directive('choicesDirective', function(){
+    return {
+      restrict: ,
+      scope: {
+        choice: =,
+
+      }
+    }
+  })
+}
