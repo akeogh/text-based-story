93311dbc8d0ed9b07a6d773c322c80e08c58b40e
Amanda Keogh <amanda.keogh@gmail.com>
2015-12-21 15:40:44 -0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var appFiles = ['server.js', __dirname + '/routes/**/*.js',
 
 var testFiles = [__dirname + '/test/**/*.js'];
 var htmlFiles = [__dirname + '/app/**/*.html'];
+var cssFiles = ['reset.css', 'base.css', 'layout.css', 'modules.css', 'state.css'];
 
 /* * * * * * * * * * * * * * * * * *
             BUILD TASKS
@@ -46,28 +47,73 @@ gulp.task('webpack:test', function() {
   .pipe(gulp.dest('test/client'));
 })
 
+gulp.task('css:dev', function() {
+  gulp.src(cssFiles)
+  .pipe(concatCSS('styles.min.css'))
+  .pipe(minifyCSS())
+  .pipe(gulp.dest('/build'));
+});
+
  /* * * * * * * * * * * * * * * * * *
             LINT TASKS
  * * * * * * * * * * * * * * * * * */
 
+gulp.task('jshint:appfiles', function() {
+  gulp.src(appFiles)
+  .pipe(jshint({
+    node: true
+  }))
+  .pipe(jshint.reporter('default'));
+});
+
+//does not include client tests
+gulp.task('jshint:testfiles', function() {
+  gulp.src('test/*.js')
+  .pipe(jshint({
+      node: true,
+      globals: {
+        before: true,
+        after: true,
+        it: true,
+        expect: true
+      }
+    }))
+  .pipe(jshint.reporter('default'));
+});
 
 
  /* * * * * * * * * * * * * * * * * *
             TEST TASKS
  * * * * * * * * * * * * * * * * * */
+gulp.task('mocha', function() {
+  gulp.src('/test/routes-test', {read: false})
+  .pipe(mocha());
+  console.log('**********************************');
+  console.log('FOR CLIENT TESTS, RUN $karma start');
+  console.log('**********************************');
 
+});
  /* * * * * * * * * * * * * * * * * *
             WATCH TASKS
  * * * * * * * * * * * * * * * * * */
 
 gulp.task('watch:build', function() {
   gulp.watch(htmlFiles, ['static:dev']);
+  gulp.watch(cssFiles, ['css:dev']);
   gulp.watch('app/**/*.js', ['webpack:dev']);
-  gulp.watch('test/client/**/*.js', ['webpack:test']);
+  //gulp.watch('test/client/**/*.js', ['webpack:test']);    UNCOMMENT WHEN CLIENT TESTS BUILT
 });
 
+gulp.task('watch:check', function() {
+  gulp.watch(appFiles, ['mocha'])
+})
+
  /* * * * * * * * * * * * * * * * * *
-              DEFAULT
+        QUICK TASKS & DEFAULT
  * * * * * * * * * * * * * * * * * */
 
+ gulp.task('jshint', ['jshint:appfiles', 'jshint:testfiles']);
+ gulp.task('build', ['static:dev', 'css:dev', 'webpack:dev']);    // ADD webpack:tests WHEN CLIENT TESTS BUILT
+
  gulp.task('default', ['watch:build']);
+
